39b333c0e5ef8be293e71c126454aa28160fe31f
Kelvin Schneider <kelvin.ks818@gmail.com>
2025-09-10 23:07:23 -0300

    feat: adicionado testes e conexão com banco de dados

--- src/main/java/com/kelvin/consulta_credito/ConsultaCreditoApplication.java
+++ src/main/java/com/kelvin/consulta_credito/ConsultaCreditoApplication.java
@@ -1,13 +1,23 @@
 package com.kelvin.consulta_credito;
 
+import com.kelvin.consulta_credito.repository.CreditoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConsultaCreditoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultaCreditoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner testDatabase(CreditoRepository repo) {
+		return args -> {
+			System.out.println("Total de créditos no banco: " + repo.count());
+		};
 	}
 
 }
